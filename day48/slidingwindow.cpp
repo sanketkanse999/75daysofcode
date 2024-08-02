ada10e72260a24f167b26e19f73f68f2c1fe7f76
sanket kanase <sanketkanse999@gmail.com>
2024-08-03 04:22:36 +0530

    slinding window

---
+++ day48/slidingwindow.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int minSwaps(vector<int>& nums) {
+         int one = 0;
+         for(auto& x : nums)
+             if(x == 1) one++;
+         if(one <= 1)
+             return 0;
+        
+         vector<int> a = nums;
+         for(auto& x : nums) {
+             a.push_back(x);
+         }
+
+         int n = a.size();
+         int ans = n;
+         int i = 0, j = 0;
+         int count = 0;
+         for(; j < n; j++) {
+             if(a[j] == 0)
+                 count++;
+             while(i < j && (j - i + 1) > one) {
+                 if(a[i] == 0)
+                     count--;
+                 i++;
+             }
+             if((j - i + 1) == one)
+                 ans = min(ans, count);
+         }
+         return ans;
+    }
+};
+
+int main() {
+    Solution solution;
+    vector<int> nums = {1, 0, 1, 0, 1};  // Example input
+    int result = solution.minSwaps(nums);
+    cout << "Minimum swaps: " << result << endl;
+    return 0;
+}
